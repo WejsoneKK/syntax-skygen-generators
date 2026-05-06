c71eaab9c8a8e2bced0afa12e0fc8072846ca126
WejsoneKK <wejsonekk@duck.com>
2026-05-06 20:59:14 +0200

    feat(plugin-core): Creaete Plugin core
    [skip ci]

--- src/main/java/eu/wejsonekk/syntaxstudio/skygengenerator/SyntaxSkygenGenerators.java
+++ src/main/java/eu/wejsonekk/syntaxstudio/skygengenerator/SyntaxSkygenGenerators.java
@@ -1,16 +1,55 @@
 package eu.wejsonekk.syntaxstudio.skygengenerator;
 
+import com.eternalcode.multification.notice.resolver.NoticeResolverRegistry;
+import com.eternalcode.multification.notice.resolver.sound.SoundAdventureResolver;
+import dev.rollczi.litecommands.LiteCommands;
+import dev.rollczi.litecommands.bukkit.LiteBukkitFactory;
+import eu.wejsonekk.syntaxstudio.skygengenerator.configuration.ConfigurationService;
+import eu.wejsonekk.syntaxstudio.skygengenerator.configuration.implementation.MessageConfig;
+import eu.wejsonekk.syntaxstudio.skygengenerator.configuration.implementation.PluginConfig;
+import eu.wejsonekk.syntaxstudio.skygengenerator.litecommand.InvalidUsageHandlerImpl;
+import eu.wejsonekk.syntaxstudio.skygengenerator.litecommand.MissingPermissionHandlerImpl;
+import eu.wejsonekk.syntaxstudio.skygengenerator.notice.NoticeService;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class SyntaxSkygenGenerators extends JavaPlugin {
 
+    private NoticeService noticeService;
+    private MessageConfig messageConfig;
+    private ConfigurationService configurationService;
+    private PluginConfig pluginConfig;
+    private LiteCommands<CommandSender> liteCommands;
+
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+
+        NoticeResolverRegistry noticeResolverRegistry = new NoticeResolverRegistry()
+                .registerResolver(new SoundAdventureResolver());
+
+        this.configurationService = new ConfigurationService(noticeResolverRegistry);
+        this.messageConfig = this.configurationService.create(MessageConfig.class, new File(this.getDataFolder(), "message.yml"));
+        this.pluginConfig = this.configurationService.create(PluginConfig.class, new File(this.getDataFolder(), "configuration.yml"));
+
+        this.noticeService = new NoticeService(this.messageConfig);
+
+
+        this.liteCommands = LiteBukkitFactory
+                .builder("syntax-skygen-generators", this)
+                .invalidUsage(new InvalidUsageHandlerImpl(this.noticeService))
+                .missingPermission(new MissingPermissionHandlerImpl(this.noticeService))
+                .build();
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (this.liteCommands != null) {
+            this.liteCommands.unregister();
+        }
     }
 }
